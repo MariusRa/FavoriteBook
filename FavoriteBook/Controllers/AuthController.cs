5d39062b9b19018b714fe8cc3b42c103be62ebd9
MariusRa <marius.raudys@stud.kitm.lt>
2022-01-16 00:53:12 +0200

    almostFinal

--- FavoriteBook/Controllers/AuthController.cs
+++ FavoriteBook/Controllers/AuthController.cs
@@ -12,13 +12,15 @@ namespace FavoriteBook.Controllers
     {
         private readonly UserManager<User> _userService;
         private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ILogger<User> _logger;
         
-        public AuthController(UserManager<User> userService, SignInManager<User> signInManager, ILogger<User> logger)
+        public AuthController(UserManager<User> userService, SignInManager<User> signInManager, ILogger<User> logger, RoleManager<IdentityRole> roleManager)
         {
             _userService = userService;
             _signInManager = signInManager;
             _logger = logger;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index() => RedirectToAction("Registration");
@@ -31,22 +33,88 @@ namespace FavoriteBook.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Registration(RegistrationViewModel model)
         {
-            if (ModelState.IsValid && (model.Password == model.ConfirmPassword))
+            if (_userService.Users.Count() > 0)
             {
-                var newUser = new User() { Email = model.Email, UserName = model.Name };
-                var result = await _userService.CreateAsync(newUser, model.Password);
-
-                if (result.Succeeded)
+                if (_userService.Users.Count() == 1)
                 {
-                    if (_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    if (ModelState.IsValid && (model.Password == model.ConfirmPassword))
                     {
-                        return RedirectToAction("ListUsers", "Administration");
-                    }
+                        var newUser = new User() { Email = model.Email, UserName = model.Name };
+                        var result = await _userService.CreateAsync(newUser, model.Password);
 
-                    await _signInManager.SignInAsync(newUser, isPersistent: false);
-                    return RedirectToAction("Index", "Home");
+                        if (result.Succeeded)
+                        {
+                            var identityRole = new IdentityRole
+                            {
+                                Name = "User"
+                            };
+
+                            var roleAdmin = await _roleManager.CreateAsync(identityRole);
+
+                            await _userService.AddToRoleAsync(newUser, identityRole.Name);
+
+                            if (_signInManager.IsSignedIn(User))
+                            {
+                                return RedirectToAction("Index", "Home");
+                            }
+
+                            await _signInManager.SignInAsync(newUser, isPersistent: false);
+                            return RedirectToAction("Index", "Home");
+                        }
+                        TempData["Errors"] = result.Errors.First().Description;
+                    }
                 }
-                TempData["Errors"] = result.Errors.First().Description;
+                else
+                {
+                    if (ModelState.IsValid && (model.Password == model.ConfirmPassword))
+                    {
+                        var newUser = new User() { Email = model.Email, UserName = model.Name };
+                        var result = await _userService.CreateAsync(newUser, model.Password);
+
+                        if (result.Succeeded)
+                        {
+                            await _userService.AddToRoleAsync(newUser, "User");
+
+                            if (_signInManager.IsSignedIn(User))
+                            {
+                                return RedirectToAction("Index", "Home");
+                            }
+
+                            await _signInManager.SignInAsync(newUser, isPersistent: false);
+                            return RedirectToAction("Index", "Home");
+                        }
+                        TempData["Errors"] = result.Errors.First().Description;
+                    }
+                }
+            }
+            else
+            {
+                if (ModelState.IsValid && (model.Password == model.ConfirmPassword))
+                {
+                    var newUser = new User() { Email = model.Email, UserName = model.Name };
+                    var result = await _userService.CreateAsync(newUser, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        var identityRole = new IdentityRole
+                        {
+                            Name = "Admin"
+                        };
+
+                        var roleAdmin = await _roleManager.CreateAsync(identityRole);
+
+                        await _userService.AddToRoleAsync(newUser, identityRole.Name);
+                                           
+                        if (_signInManager.IsSignedIn(User) && await _userService.IsInRoleAsync(newUser, "Admin"))
+                        {
+                            return RedirectToAction("ListBooks", "Book");
+                        }
+
+                        await _signInManager.SignInAsync(newUser, isPersistent: false);
+                        return RedirectToAction("ListBooks", "Book");
+                    }
+                    TempData["Errors"] = result.Errors.First().Description;
+                }
             }
             return View(model);
         }
@@ -73,13 +141,17 @@ namespace FavoriteBook.Controllers
 
                     if (result.Succeeded)
                     {
-                        if (!string.IsNullOrEmpty(model.ReturnUrl))
+                        if (await _userService.IsInRoleAsync(myUser, "Admin"))
+                        {
+                            _logger.LogInformation("Admin logged in.");
+                            return RedirectToAction("ListBooks", "Book");
+                        }
+                        else if (!string.IsNullOrEmpty(model.ReturnUrl))
                         {
                             return Redirect(model.ReturnUrl);
                         }
                         else
                         {
-                            
                             _logger.LogInformation("User logged in.");
                             return RedirectToAction("Index", "Home");
                         }
@@ -87,8 +159,7 @@ namespace FavoriteBook.Controllers
                 }
                 ViewBag.Message = "Email or password is wrong!";
             }
-
-            
+ 
             return View(model);
         }
 
